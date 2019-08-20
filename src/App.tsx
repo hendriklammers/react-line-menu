4d1dd44ad0c3460507a4eb1c896f951c4cc7509e
Hendrik Lammers <lammers@gmail.com>
2019-08-20 19:08:24 +0700

    Use react spring to animate line

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+// react-spring causes tsserver performance problems
+// https://github.com/react-spring/react-spring/issues/613
+import { useSpring, animated } from 'react-spring'
+import styled from 'styled-components'
 
-const App: React.FC = () => {
+const menuItems = [
+  { label: 'Home', active: true },
+  { label: 'About', active: false },
+  { label: 'Blog', active: false },
+  { label: 'Contact', active: false },
+]
+
+const Menubar = styled.nav`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 20px 10px;
+`
+
+const Line = styled(animated.div)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: #000;
+  width: 100px;
+  height: 2px;
+`
+
+const Link = styled.a`
+  margin: 0 10px;
+  font-size: 30px;
+  color: #000;
+  text-decoration: none;
+`
+
+const App = () => {
+  const [animatedProps, setAnimatedProps] = useSpring(() => ({
+    transformOrigin: 'top left',
+    transform: 'translateX(0px) scaleX(1)',
+    config: { mass: 1, tension: 120, friction: 20 },
+  }))
+
+  const linkRefs = useRef<(HTMLAnchorElement)[]>([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Menubar>
+      {menuItems.map(({ label }, index) => {
+        return (
+          <Link
+            key={index}
+            href="/"
+            ref={ref => linkRefs.current.push(ref as HTMLAnchorElement)}
+            onClick={event => {
+              event.preventDefault()
+              const ref = linkRefs.current[index]
+              const left = ref.offsetLeft
+              const scale = ref.getBoundingClientRect().width / 100
+              setAnimatedProps({
+                transform: `translateX(${left}px) scaleX(${scale})`,
+              })
+            }}
+          >
+            {label}
+          </Link>
+        )
+      })}
+      <Line style={animatedProps} />
+    </Menubar>
+  )
 }
 
-export default App;
+export default App

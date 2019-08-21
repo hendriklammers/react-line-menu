eae6e071525f02ae7fd4df37a6990697a8a37132
Hendrik Lammers <lammers@gmail.com>
2019-08-21 18:46:00 +0700

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,76 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link as RouterLink,
+  withRouter,
+  RouteProps,
+} from 'react-router-dom'
 import LineMenu from './LineMenu'
 import styled from 'styled-components'
 
-const Link = styled.a`
-  margin: 0 10px;
+const Link = styled(RouterLink)`
   font-size: 30px;
   color: #000;
   text-decoration: none;
 `
 
-const App = () => {
+const Home = () => (
+  <section>
+    <h2>Home</h2>
+  </section>
+)
+
+const About = () => (
+  <section>
+    <h2>About</h2>
+  </section>
+)
+
+const Blog = () => (
+  <section>
+    <h2>Blog</h2>
+  </section>
+)
+
+const Contact = () => (
+  <section>
+    <h2>Contact</h2>
+  </section>
+)
+
+const Navigation = withRouter(({ location }: RouteProps) => {
   const [activeMenuItem, setActiveMenuItem] = useState(0)
 
-  const clickHandler = (index: number, event?: MouseEvent) => {
-    if (event) {
-      event.preventDefault()
-    }
+  const clickHandler = (index: number) => {
     setActiveMenuItem(index)
   }
 
+  // if (location) {
+  //   console.log(location.pathname.match(/^\/([a-zA-Z]+)/))
+  // }
+
   return (
-    <div>
-      <LineMenu active={activeMenuItem} clickHandler={clickHandler}>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/contact">Contact</Link>
-      </LineMenu>
-    </div>
+    <LineMenu active={activeMenuItem} clickHandler={clickHandler} space={20}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/contact">Contact</Link>
+    </LineMenu>
+  )
+})
+
+const App = () => {
+  return (
+    <Router>
+      <Navigation />
+      <section>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+      </section>
+    </Router>
   )
 }
 

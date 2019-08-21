6855d244b28bd17dde8f5805e03a46cf23bcac7a
Hendrik Lammers <lammers@gmail.com>
2019-08-21 20:36:43 +0700

    Add Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import React, { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link as RouterLink,
-  withRouter,
-  RouteProps,
-} from 'react-router-dom'
-import LineMenu from './LineMenu'
-import styled from 'styled-components'
-
-const Link = styled(RouterLink)`
-  font-size: 30px;
-  color: #000;
-  text-decoration: none;
-`
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navigation from './Navigation'
 
 const Home = () => (
   <section>
@@ -38,27 +25,6 @@ const Contact = () => (
     <h2>Contact</h2>
   </section>
 )
-
-const Navigation = withRouter(({ location }: RouteProps) => {
-  const [activeMenuItem, setActiveMenuItem] = useState(0)
-
-  const clickHandler = (index: number) => {
-    setActiveMenuItem(index)
-  }
-
-  // if (location) {
-  //   console.log(location.pathname.match(/^\/([a-zA-Z]+)/))
-  // }
-
-  return (
-    <LineMenu active={activeMenuItem} clickHandler={clickHandler} space={20}>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/blog">Blog</Link>
-      <Link to="/contact">Contact</Link>
-    </LineMenu>
-  )
-})
 
 const App = () => {
   return (
---
+++ src/Navigation.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { withRouter, RouteProps, Link } from 'react-router-dom'
+import LineMenu from './LineMenu'
+
+const MenuLink = styled(Link)`
+  font-size: 30px;
+  color: #000;
+  text-decoration: none;
+`
+
+type MenuItem = {
+  to: string
+  label: string
+}
+
+const menuItems: MenuItem[] = [
+  { to: '/', label: 'Home' },
+  { to: '/about', label: 'About' },
+  { to: '/blog', label: 'Blog' },
+  { to: '/contact', label: 'Contact' },
+]
+
+const Navigation = withRouter(({ location }: RouteProps) => {
+  const [activeMenuItem, setActiveMenuItem] = useState(0)
+
+  const clickHandler = (index: number) => {
+    setActiveMenuItem(index)
+  }
+
+  useEffect(() => {
+    if (location) {
+      const index = menuItems.findIndex(({ to }) => to === location.pathname)
+      if (index >= 0) {
+        setActiveMenuItem(index)
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <LineMenu active={activeMenuItem} clickHandler={clickHandler} space={20}>
+      {menuItems.map(({ to, label }, index) => (
+        <MenuLink to={to} key={index}>
+          {label}
+        </MenuLink>
+      ))}
+    </LineMenu>
+  )
+})
+
+export default Navigation

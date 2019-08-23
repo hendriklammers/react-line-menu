48ed026a17ea3a1ae6498403d761d180de6828a6
Hendrik Lammers <lammers@gmail.com>
2019-08-23 18:14:57 +0700

    Use __tests__ folder

--- src/__tests__/LineMenu.tsx
+++ src/__tests__/LineMenu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import LineMenu from './LineMenu'
+import LineMenu from '../LineMenu'
 
 describe('LineMenu component', () => {
   it('renders correctly', () => {
